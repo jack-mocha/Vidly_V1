031faf58b8d21f16cef9048f5ca8cab42f37e354
jackhsu0225 <jackhsu0225@gmail.com>
2017-10-04 10:55:08 -0400

    Exercise3: [1] Display membership type [2] Display customers birthdate [3] Display list of movies and details

--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -5,15 +5,20 @@ using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
 using Vidly.ViewModels;
-
+using System.Data.Entity;
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        private ApplicationDbContext _context;
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
         // GET: Movies
         public ActionResult Index()
         {
-            var movies = GetMovies();
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
             MoviesViewModel viewModel = new MoviesViewModel()
             {
                 Movies = movies
@@ -23,7 +28,7 @@ namespace Vidly.Controllers
         }
         public ActionResult Details(int id)
         {
-            var movie = GetMovies().SingleOrDefault(m => m.Id == id);
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
 
             if (movie == null)
                 return HttpNotFound();
