6ba506e8507999ddd381259e49727eea9fb06256
jackhsu0225 <jackhsu0225@gmail.com>
2017-11-02 16:46:55 -0400

    Implemented CRUD operation around movie [1] GetMovies [2] GetMovies(id) [3] CreateMovie(movieDto) [4] UpdateMovie(id, movieDto) [5] DeleteMovie(id)

--- Vidly/Vidly/Dtos/CustomerDto.cs
+++ Vidly/Vidly/Dtos/CustomerDto.cs
@@ -19,7 +19,7 @@ namespace Vidly.Dtos
 
         public byte MembershipTypeId { get; set; }
 
-        //[Min18YearsIfAMember]
+        //[Min18YearsIfAMember] //Disabled - Ideally we should have only 1 way to create a customer, but now we have both mvc action and api. This is disabled because the custom validation is using Customer instead of CustomerDto
         public DateTime? BirthDate { get; set; }
     }
 }
